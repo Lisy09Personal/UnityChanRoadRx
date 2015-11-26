545347938b878b730f13aecd78cbb220d91266d0
lisy09 <lisy09.thu@gmail.com>
2015-11-26 16:17:40 +0900

    init

---
+++ Assets/Scripts/Controller/IntroController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+public class IntroController : ObservableMonoBehaviour
+{
+    public override void Awake()
+    {
+		// Intro Scene >>(3s)>> Stage Scene
+        Observable.Timer(System.TimeSpan.FromSeconds(3f))
+            .Subscribe(_ =>
+            {
+                Application.LoadLevel("Stage");
+            });
+
+        base.Awake();
+    }
+}
